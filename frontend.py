5fd317791fdbc95130b2b98467e142eec814d8de
pss0204 <ss0204.park@gmail.com>
2024-12-12 15:39:54 +0900

    2레이어 별로 지원

--- frontend.py
+++ frontend.py
@@ -2,18 +2,30 @@ from ir.ir_node import IRNode
 import numpy as np
 
 def build_toy_model_ir(model):
+    ir_nodes = []
     input_node = IRNode(
-        op_type='Input', 
+        op_type='Input',
         inputs=[], 
         params={'shape': (2,)}
     )
-    linear_node = IRNode(
-        op_type='Linear', 
-        inputs=[input_node], 
-        params={'W': model.W, 'b': model.b}
-    )
-    relu_node = IRNode(
-        op_type='Relu', 
-        inputs=[linear_node]
-    )
-    return [input_node, linear_node, relu_node]
+    ir_nodes.append(input_node)
+    
+    previous_node = input_node
+    for layer in model.layers:
+        if layer['type'] == 'Linear':
+            linear_node = IRNode(
+                op_type='Linear',
+                inputs=[previous_node],
+                params={'W': layer['params']['W'], 'b': layer['params']['b']}
+            )
+            ir_nodes.append(linear_node)
+            previous_node = linear_node
+        elif layer['type'] == 'Relu':
+            relu_node = IRNode(
+                op_type='Relu',
+                inputs=[previous_node]
+            )
+            ir_nodes.append(relu_node)
+            previous_node = relu_node
+        # 다른 레이어 타입 추가 가능
+    return ir_nodes
--- toy_model.py
+++ toy_model.py
@@ -2,12 +2,18 @@ import numpy as np
 
 class ToyModel:
     def __init__(self):
-        self.W = np.random.randn(1, 2)
-        self.b = np.random.randn(1)
+        self.layers = [
+            {'type': 'Linear', 'params': {'W': np.random.randn(1, 2), 'b': np.random.randn(1)}},
+            {'type': 'Relu'}
+        ]
 
     def relu(self, x):
         return np.maximum(x, 0)
 
     def forward(self, x):
-        linear = np.dot(self.W, x) + self.b
-        return self.relu(linear)
+        for layer in self.layers:
+            if layer['type'] == 'Linear':
+                x = np.dot(layer['params']['W'], x) + layer['params']['b']
+            elif layer['type'] == 'Relu':
+                x = self.relu(x)
+        return x
